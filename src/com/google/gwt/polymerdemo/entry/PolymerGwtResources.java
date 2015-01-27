ef55d34f410f1f26c6ea5d30c0409a74bd845b8f
Chris DiGiano <digi@google.com>
2015-01-28 00:14:56 +0100

    Use star for a button

--- src/com/google/gwt/polymerdemo/entry/PolymerGwtResources.java
+++ src/com/google/gwt/polymerdemo/entry/PolymerGwtResources.java
@@ -2,12 +2,15 @@ package com.google.gwt.polymerdemo.entry;
 
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.resources.client.ImageResource;
 
 public interface PolymerGwtResources extends ClientBundle {
 
   @Source("PolymerGwt.css")
   Css css();
-  
+ 
+  ImageResource star();
+
   interface Css extends CssResource {
     String drawer();
     String main();
